5b21513473632aeae82b54555c8cc8f08ca90555
segfaulty1 <segfaulty@proton.me>
2023-11-01 14:18:12 +0100

    enhanced-ui: added responsiveness and notifications.

--- client/src/components/shared/header.tsx
+++ client/src/components/shared/header.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
-import { Camera, Moon, MoonIcon, Sun, SunDim } from 'lucide-react';
+import { Link, NavLink } from 'react-router-dom';
+import { Camera, Moon, MoonIcon, Sun, SunDim, X, MenuIcon } from 'lucide-react';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../../state/store';
 import { actions } from '../../state/userPreferences/userPreferences';
+import { Tooltip } from 'react-tooltip';
 
 const Header = () => {
     const userPreferences = useSelector((state: RootState) => state.theme);
@@ -10,12 +12,97 @@ const Header = () => {
 
     const changeTheme = () => dispatch(actions.toggleTheme())
 
+    const menuDialogRef = React.useRef();
+
     return (
-        <header className='header'>
-            <button className={`themeButton`} onClick={changeTheme}>
-                {userPreferences.theme === 'dark' ? <Moon size={18} /> : <Sun size={18} />}
+        <>
+            <header className='wide-screen-menu'>
+                <nav>
+                    <ul>
+                        <li>
+                            <NavLink
+                                to="/template"
+                                className={({ isActive }) =>
+                                    isActive ? "active" : ""
+                                }
+                            >
+                                template
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink
+                                to="/resumes"
+                                className={({ isActive }) =>
+                                    isActive ? "active" : ""
+                                }
+                            >
+                                resumes
+                            </NavLink>
+                        </li>
+                        <li>
+                            <button
+                                className={`themeButton`}
+                                onClick={changeTheme}
+                                data-tooltip-id='theme-color'
+                                data-tooltip-content='Change Theme'
+                                >
+                                {userPreferences.theme === 'dark' ? <Moon size={18} /> : <Sun size={18} />}
+                            </button>
+                            <Tooltip id='theme-color' />
+                        </li>
+                    </ul>
+                </nav>
+            </header >
+            <button
+                data-modal-show
+                onClick={() => {
+                    menuDialogRef.current?.showModal();
+                }}>
+                <MenuIcon size={30} />
             </button>
-        </header>
+            <dialog
+                ref={menuDialogRef}
+                data-modal
+                className='small-screen-menu'
+            >
+                <button
+                    data-modal-close
+                    onClick={() => {
+                        menuDialogRef.current?.close();
+                    }}>
+                    <X />
+                </button>
+                <nav>
+                    <ul>
+                        <li>
+                            <NavLink
+                                to="/template"
+                                className={({ isActive }) =>
+                                    isActive ? "active" : ""
+                                }
+                            >
+                                template
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink
+                                to="/resumes"
+                                className={({ isActive }) =>
+                                    isActive ? "active" : ""
+                                }
+                            >
+                                resumes
+                            </NavLink>
+                        </li>
+                        <li>
+                            <button className={`themeButton`} onClick={changeTheme}>
+                                {userPreferences.theme === 'dark' ? <Moon size={18} /> : <Sun size={18} />}
+                            </button>
+                        </li>
+                    </ul>
+                </nav>
+            </dialog >
+        </>
     )
 }
 
--- client/src/components/shared/layout.tsx
+++ client/src/components/shared/layout.tsx
@@ -1,15 +1,26 @@
 import React, { Children } from 'react'
 import Header from './header';
+import { Outlet } from 'react-router-dom';
 
-interface LayoutProps {
-  children: React.ReactNode
-}
+import { useSelector } from 'react-redux';
+import { RootState } from '../../state/store';
 
-const Layout = (props:LayoutProps) => {
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+interface LayoutProps { }
+const Layout = (props: LayoutProps) => {
+
+  const { theme } = useSelector((state: RootState) => state.theme);
+
   return (
     <>
-      <Header/>
-      {props.children}
+      <ToastContainer toastStyle={{
+        backgroundColor: theme == 'dark' ? '#1a1a1a' : '#f9f9f9',
+        color: theme == 'dark' ? '#fff' : '#213547',
+      }} />
+      <Header />
+      <Outlet />
     </>
   )
 }
